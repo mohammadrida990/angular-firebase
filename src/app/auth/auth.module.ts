da650a928f202f4dc27e6ff634b4ef9d80816527
rida <rida@hotmail.com>
2020-10-26 23:04:56 +0200

    finish login html

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,17 +5,22 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports:[
     LoginComponent,
     RegisterComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm = new FormGroup({
+    email : new FormControl(),
+    password : new FormControl()
+  });
   constructor() { }
-
+  
   ngOnInit() {
   }
 
+  onSubmit(){
+
+  }
+
+  
 }
